d28f72f02710b06864ab377d0f9471b1ea778c27
Luthfi Irsyadurrafi <luthfiirsya@MacBook-Air-Luthfi.local>
2025-07-29 17:10:36 +0800

    project aktualisasi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+
+@section('judul')
+Daftar Data
+@endsection
+
+@section('content')
+
+<a href="/agenda">Agenda</a><br><br>
+<a href="/bukutamu">Buku Tamu</a><br><br>
+
+@endsection
