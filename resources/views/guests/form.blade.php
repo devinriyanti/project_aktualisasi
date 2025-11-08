cadac935b52af0e18b693abc3119f7329f25429c
Given Jeremia <jeremiagiven@gmail.com>
2025-11-08 14:51:31 +0700

    dududu

--- resources/views/guests/form.blade.php
+++ resources/views/guests/form.blade.php
@@ -72,7 +72,7 @@
 
                         @if(request()->is_with_date == "true")
                             <div class="mb-4">
-                                <label for="date" class="block text-sm font-medium text-gray-700">Tanggal Anjing</label>
+                                <label for="date" class="block text-sm font-medium text-gray-700">Custom Tanggal Waktu</label>
                                 <input type="datetime-local" id="date" name="date"
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                             </div>
